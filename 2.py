cdf4ae7ec0c9e4f909b4017aa48810760cb07ca9
Airat <101736633+yak0da@users.noreply.github.com>
2025-12-28 11:48:32 +0300

    Update 2.py

--- 2.py
+++ 2.py
@@ -1,71 +1,33 @@
 def sizer(cls):
-    """
-    Декоратор класса, добавляющий свойство size.
-    size возвращает len(obj), если объект имеет длину,
-    иначе abs(obj), если определен модуль,
-    иначе 0.
-    """
-    
-    class SizedClass(cls):
+    class SizerWrapper(cls):
         def __init__(self, *args, **kwargs):
-            self._size = None  # Собственное значение size
             super().__init__(*args, **kwargs)
-        
+            object.__setattr__(self, '_size_set', False)
+            object.__setattr__(self, '_size_value', None)
+
         @property
         def size(self):
-            # Если установлено собственное значение
-            if self._size is not None:
-                return self._size
-            
-            # Пытаемся вернуть длину
+            if self._size_set:
+                return self._size_value
             try:
                 return len(self)
-            except (TypeError, AttributeError):
-                pass
-            
-            # Пытаемся вернуть модуль
-            try:
-                return abs(self)
-            except (TypeError, AttributeError):
-                pass
-            
-            # По умолчанию 0
-            return 0
-        
+            except TypeError:
+                try:
+                    return abs(self)
+                except TypeError:
+                    return 0
+
         @size.setter
         def size(self, value):
-            self._size = value
-        
+            object.__setattr__(self, '_size_set', True)
+            object.__setattr__(self, '_size_value', value)
+
         @size.deleter
         def size(self):
-            self._size = None
-    
-    # Сохраняем оригинальное имя класса
-    SizedClass.__name__ = cls.__name__
-    
-    return SizedClass
+            object.__setattr__(self, '_size_set', False)
+            object.__setattr__(self, '_size_value', None)
 
-
-# Тестовый код
-if __name__ == "__main__":
-    @sizer
-    class S(list):
-        pass
-
-    @sizer
-    class N(complex):
-        pass
-
-    @sizer
-    class E(Exception):
-        pass
-
-    for obj in S("QWER"), N(3+4j), E("Exceptions know no lengths!"):
-        print(obj, obj.size)
-    
-    p = S(range(10, 15))
-    print(p.size)
-    p.size = p.pop()
-    print(p.size)
-    del p.size
-    print(p.size)
+    SizerWrapper.__name__ = cls.__name__
+    SizerWrapper.__qualname__ = cls.__qualname__
+    SizerWrapper.__module__ = cls.__module__
+    return SizerWrapper
